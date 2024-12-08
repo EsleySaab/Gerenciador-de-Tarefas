0a32ed7a4fc3837b9d46f9b0d8852ac144f145cc
EsleySaab <esleysantana.sa@gmail.com>
2024-12-08 18:11:14 -0300

    creating controller and authentication route

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -36,7 +36,7 @@ class UsersController {
 
     const { password: _, ...userWithoutPassword } = user
 
-    return response.json(userWithoutPassword)
+    return response.status(201).json(userWithoutPassword)
   }
 }
 
