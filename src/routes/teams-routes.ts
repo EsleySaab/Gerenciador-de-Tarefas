bca7fb4a4018ee41f2e40833f2967630bb1e44c1
EsleySaab <esleysantana.sa@gmail.com>
2024-12-08 20:02:46 -0300

    checking authorization

--- src/routes/teams-routes.ts
+++ src/routes/teams-routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express"
 
 import { TeamsController } from "@/controllers/teams-controller"
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const teamsRoutes = Router()
 const teamsController = new TeamsController()
 
-teamsRoutes.use(ensureAuthenticated)
+teamsRoutes.use(ensureAuthenticated, verifyUserAuthorization(["admin"]))
 teamsRoutes.post("/", teamsController.create)
 
 export { teamsRoutes }
